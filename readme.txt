1ac26221a08dcd9d7183f8df0f9b43915de36888
Alejandro De León Languré <106360071+langure@users.noreply.github.com>
2024-05-16 14:59:15 +0900

    Added backend

--- readme.txt
+++ readme.txt
@@ -1,2 +1,18 @@
 change node version:
 nvm use 18
+
+running mongo on local docker:
+docker pull mongo:latest
+docker run --name mongodb-container --network=host -d mongo
+docker ps
+docker inspect mongodb-container | grep "IPAddress"
+
+URL:
+mongodb://<mongodb-container-ip-address>:27017/mydatabase
+
+for example
+mongodb://72.17.0.2:27017/metadatos
+
+or locally
+brew tap mongodb/brew
+brew install mongodb-community
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DATABASE_URL;
+
+// Connect to MongoDB
+mongoose.connect(databaseUrl)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.get('/test', (req, res) => {
-  res.send('Hello, world!');
+  // about to connect to MongoDB
+  console.log("connecting to MongoDB... with db url: " + databaseUrl);
+  if (mongoose.connection.readyState === 1) { // 1 means connected
+    res.send('MongoDB connection is established');
+  } else {
+    res.status(500).send('MongoDB connection is not established'); // Send 500 Internal Server Error
+  }
 });
 
 app.listen(port, () => {
